b8855156e0b8ac084a50a197ceac961c28631bee
eloquentarduino <eloquentarduino@gmail.com>
2022-07-12 19:11:13 +0200

    bump dist to 0.0.4

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from distutils.core import setup
 
-packages=["everywhereml", "everywhereml.plot", "everywhereml.sklearn", "everywhereml.sklearn.tree", "everywhereml.sklearn.tree.templates", "everywhereml.sklearn.ensemble", "everywhereml.sklearn.ensemble.templates", "everywhereml.get_started", "everywhereml.code_generators", "everywhereml.code_generators.jinja", "everywhereml.code_generators.jinja.filters", "everywhereml.templates", "everywhereml.templates.cpp", "everywhereml.preprocessing", "everywhereml.preprocessing.templates", "everywhereml.data", "everywhereml.data.collect"]
-data=["sklearn/tree/templates/tree.cpp.jinja", "sklearn/tree/templates/DecisionTreeClassifier.cpp.jinja", "sklearn/ensemble/templates/tree.cpp.jinja", "sklearn/ensemble/templates/RandomForestClassifier.cpp.jinja", "templates/latency.cpp.jinja", "templates/vote.cpp.jinja", "templates/class_map.cpp.jinja", "templates/BaseClassifier.cpp.jinja", "preprocessing/templates/Step.cpp.jinja", "preprocessing/templates/Pipeline.cpp.jinja", "preprocessing/templates/SpectralFeatures.cpp.jinja", "preprocessing/templates/Window.cpp.jinja", "preprocessing/templates/MinMaxScaler.cpp.jinja"]
+packages=["everywhereml", "everywhereml.plot", "everywhereml.tests", "everywhereml.tests.runtime", "everywhereml.tests.sklearn", "everywhereml.tests.sklearn.ensemble", "everywhereml.tests.templates", "everywhereml.tests.preprocessing", "everywhereml.tests.preprocessing.image", "everywhereml.tests.data", "everywhereml.sklearn", "everywhereml.sklearn.tree", "everywhereml.sklearn.tree.templates", "everywhereml.sklearn.ensemble", "everywhereml.sklearn.ensemble.templates", "everywhereml.get_started", "everywhereml.code_generators", "everywhereml.code_generators.jinja", "everywhereml.code_generators.jinja.filters", "everywhereml.templates", "everywhereml.templates.cpp", "everywhereml.preprocessing", "everywhereml.preprocessing.image", "everywhereml.preprocessing.image.object_detection", "everywhereml.preprocessing.image.object_detection.templates", "everywhereml.preprocessing.image.templates", "everywhereml.preprocessing.image.transform", "everywhereml.preprocessing.image.transform.templates", "everywhereml.preprocessing.templates", "everywhereml.data", "everywhereml.data.collect"]
+data=["tests/templates/pipeline.cpp.jinja", "tests/templates/classifier.cpp.jinja", "sklearn/tree/templates/tree.cpp.jinja", "sklearn/tree/templates/DecisionTreeClassifier.cpp.jinja", "sklearn/ensemble/templates/tree.cpp.jinja", "sklearn/ensemble/templates/RandomForestClassifier.cpp.jinja", "templates/latency.cpp.jinja", "templates/vote.cpp.jinja", "templates/class_map.cpp.jinja", "templates/BaseClassifier.cpp.jinja", "preprocessing/image/object_detection/templates/HogPipeline.cpp.jinja", "preprocessing/image/object_detection/templates/BaseObjectDetectionPipeline.cpp.jinja", "preprocessing/image/templates/BaseImageStep.cpp.jinja", "preprocessing/image/templates/HOG.cpp.jinja", "preprocessing/image/templates/LBP.cpp.jinja", "preprocessing/image/transform/templates/Resize.cpp.jinja", "preprocessing/templates/Step.cpp.jinja", "preprocessing/templates/Pipeline.cpp.jinja", "preprocessing/templates/SpectralFeatures.cpp.jinja", "preprocessing/templates/Window.cpp.jinja", "preprocessing/templates/MinMaxScaler.cpp.jinja"]
 
 setup(
   name='everywhereml',
   packages=packages,
-  version='0.0.3',
+  version='0.0.4',
   license='MIT',
   description='Train ML in Python, run everywhere',
   author='Simone Salerno',
   author_email='support@eloquentarduino.com',
   url='https://github.com/eloquentarduino/everywhereml',
-  download_url='https://github.com/eloquentarduino/everywhereml/blob/master/dist/everywhereml-0.0.3.tar.gz?raw=true',
+  download_url='https://github.com/eloquentarduino/everywhereml/blob/master/dist/everywhereml-0.0.4.tar.gz?raw=true',
   keywords=[
     'ML',
     'machine learning'
@@ -22,8 +22,12 @@ setup(
     'pandas',
     'seaborn',
     'scikit-learn',
+    'scikit-image',
     'Jinja2',
     'cached-property',
+    'umap-learn',
+    'umap-learn[plot]',
+    'python-slugify'
   ],
   package_data={
     'everywhereml': data
