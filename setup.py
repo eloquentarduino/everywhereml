f92be2688433077de220ccba517222fb9f06087b
eloquentarduino <eloquentarduino@gmail.com>
2023-03-25 17:07:12 +0100

    bump dist to 0.2.21

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ data=["tests/templates/pipeline.cpp.jinja", "tests/templates/classifier.cpp.jinj
 setup(
   name='everywhereml',
   packages=packages,
-  version='0.2.20',
+  version='0.2.21',
   license='MIT',
   description='Train ML in Python, run everywhere',
   author='Simone Salerno',
   author_email='support@eloquentarduino.com',
   url='https://github.com/eloquentarduino/everywhereml',
-  download_url='https://github.com/eloquentarduino/everywhereml/blob/master/dist/everywhereml-0.2.20.tar.gz?raw=true',
+  download_url='https://github.com/eloquentarduino/everywhereml/blob/master/dist/everywhereml-0.2.21.tar.gz?raw=true',
   keywords=[
     'ML',
     'machine learning'
